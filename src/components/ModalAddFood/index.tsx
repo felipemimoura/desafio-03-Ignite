dc12d8c6c625ac3da99aa3caffb75f5c72d5f27b
Felipe Moura <felipe.moura@interprocess.com.br>
2021-09-20 21:14:54 -0300

    refactor: ajuste na tipagem e no elemento modalAddFood

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,4 +1,6 @@
-import { FormEvent, useEffect, useRef, useState } from 'react';
+import { FormEvent, useRef, useState } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+import Input from '../Input/index';
 import Modal from '../Modal';
 import { Form } from './styles';
 
@@ -11,19 +13,22 @@ interface ModalAddFoodProps {
 
 
 export function ModalAddFood({isOpen, handleAddFood, setIsOpen}: ModalAddFoodProps) {
-  const [open, setOpen] = useState(false)
+  const [image, setImage] = useState('')
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [description, setDescription] = useState('')
   const formRef = useRef(null)
 
-  useEffect(() => {
-    setOpen(isOpen)
-  },[])
 
   const handleSubmit = async (data: FormEvent) => {
-    data.preventDefault()
-    console.log('data', data)
 
-    // handleAddFood(data);
+    handleAddFood();
     setIsOpen();
+    
+    setImage('')
+    setName('')
+    setPrice('')
+    setDescription('')
   };
 
 
@@ -32,18 +37,18 @@ export function ModalAddFood({isOpen, handleAddFood, setIsOpen}: ModalAddFoodPro
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
         <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Novo Prato</h1>
-          {/* <Input name="image" placeholder="Cole o link aqui" />
+          <Input name="image" placeholder="Cole o link aqui" value={image} onChange={(event) => setImage(event?.target.value)} />
 
-          <Input name="name" placeholder="Ex: Moda Italiana" />
-          <Input name="price" placeholder="Ex: 19.90" />
+          <Input name="name" placeholder="Ex: Moda Italiana" value={name} onChange={(event) => setName(event?.target.value)} />
+          <Input name="price" placeholder="Ex: 19.90" value={price} onChange={(event) => setPrice(event?.target.value)}/>
 
-          <Input name="description" placeholder="Descrição" />
+          <Input name="description" placeholder="Descrição" value={description} onChange={(event) => setDescription(event?.target.value)} />
           <button type="submit" data-testid="add-food-button">
             <p className="text">Adicionar Prato</p>
             <div className="icon">
               <FiCheckSquare size={24} />
             </div>
-          </button> */}
+          </button>
         </Form>
       </Modal>
     );
