f142e4ee36936c4163c4952674a77b09812c6bc6
Felipe Moura <felipe.moura@interprocess.com.br>
2021-09-21 19:48:53 -0300

    refactor: alterado componente de classe para funcional e adicionado typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,29 @@
-import {
+import React, {
   useEffect,
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
+  SVGAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Icon extends SVGAttributes<SVGElement> {
+  size: number;
+}
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+  name: string;
+  icon?: Icon;
+  placeholder:string;
+  value: string | number;
+}
+
+const Input = ({name, icon: Icon, ...rest} : InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -20,9 +33,11 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputFocus = useCallback(() => {
     setIsFocused(true);
   }, []);
-
+  
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
+ 
+    
 
     setIsFilled(!!inputRef.current?.value);
   }, []);
@@ -35,9 +50,11 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     });
   }, [fieldName, registerField]);
 
+
+
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {/* {Icon && <Icon  />} */}
 
       <input
         onFocus={handleInputFocus}
---
+++ src/hooks/useOrder.tsx
@@ -0,0 +1,47 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import api from "../services/api";
+
+interface Order{
+  id: number;
+  name: string;
+  available: boolean;
+  description: string;
+  image: string;
+  price:string
+}
+
+type OrderInput = Omit<Order,'id'| 'available'>
+
+interface OrdersContext {
+  orders: Order[]
+  createOrder: (order: OrderInput) => Promise<void>
+}
+
+interface OrderProviderProps {
+  children: ReactNode
+}
+
+const OrderContext = createContext<OrdersContext>({} as OrdersContext)
+
+export const OrderProvider = ({children}: OrderProviderProps) => {
+  const [orders, setOrders] = useState<Order[]>([])
+
+  async function createOrder(ordersInput: OrderInput){
+    const response = await api.post('foods', {...ordersInput, available: true})
+    setOrders([...orders, response.data])
+  }
+  useEffect(() => {
+    api.get('/foods').then(response => setOrders(response.data))
+  },[orders])
+
+  return(
+    <OrderContext.Provider value={{orders,createOrder}} >{children}</OrderContext.Provider>
+  )
+}
+
+export const useOrders = () => {
+  const context = useContext(OrderContext)
+
+  return context
+}
+
