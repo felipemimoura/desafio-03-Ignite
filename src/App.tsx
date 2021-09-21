07916969ac56205bcb3444ab1ba69e5482b2d55d
Felipe Moura <felipe.moura@interprocess.com.br>
2021-09-21 19:39:18 -0300

    feat: criado contexto para pegar lista de restaurantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { OrderProvider } from './hooks/useOrder';
 
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
 const App = () => (
-  <>
+  <OrderProvider>
     <GlobalStyle />
     <Router>
       <Routes />
     </Router>
-  </>
+  </OrderProvider>
 );
 
 export default App;
